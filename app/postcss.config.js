53816c642a1fc69c4c0a4addce133a1ae7213ae4
Sukhans Asrani <sukhansnz@gmail.com>
2020-09-01 20:11:01 +1200

    Update postcss.config.js

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -1,2 +1,5 @@
-import tailwindcss from "tailwindcss";
-export const plugins = [tailwindcss("./tailwind.js"), require("autoprefixer")];
+/* eslint-disable @typescript-eslint/no-var-requires */
+const tailwindcss = require("tailwindcss");
+module.exports = {
+    plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
+};
