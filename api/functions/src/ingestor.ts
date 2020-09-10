97e9e9ade9734b687a3e675f07df6cca8b7660db
Alex Monk <amonk446@gmail.com>
2020-09-10 21:40:27 +1200

    Added data splitter that isn't working yet

--- api/functions/src/ingestor.ts
+++ api/functions/src/ingestor.ts
@@ -3,6 +3,7 @@
 
 import csvParser from 'csv-parser';
 import fs from 'fs';
+import { raw } from 'express';
 
 const results: any[] = [];
 
@@ -12,10 +13,21 @@ fs.createReadStream('2004.02.12.10.32.39')
   .on('end', () => {
     console.log(results);
     // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
+    //   '-0.049\t-0.071\t-0.132\t-0.010': '-0.042\t-0.073\t-0.007\t-0.105'
+    //   '-0.049\t-0.071\t-0.132\t-0.010': '0.015\t0.000\t0.007\t0.000'
+    //    First column is just the first entry. Is currently getting ignored.
     // ]
   });
+
+const rawDataFirstColumn: number[] = [];
+
+for (let i = 0; i < results.length; i++) {
+  // rawDataFirstColumn.push(results[i][1].split('\t', 999999999)[0]);
+  rawDataFirstColumn.push(results[i][1].split('\t', 999999999));
+}
+
+console.log('raw data from first column:');
+console.log(rawDataFirstColumn);
 
 /* calculate rms value. see 4.1.2 of the full research doc (pg 9) for info on how this is calculated*/
 
