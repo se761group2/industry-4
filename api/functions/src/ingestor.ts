506e69645a2f994845ae98f34fc973d5153c343b
Alex Monk <amonk446@gmail.com>
2020-09-12 15:17:14 +1200

    calculates correct RMS value for entire file

--- api/functions/src/ingestor.ts
+++ api/functions/src/ingestor.ts
@@ -6,7 +6,7 @@ import fs from 'fs';
 import { raw } from 'express';
 
 const results: any[] = [];
-const rawDataFirstColumn: string[] = [];
+const rawDataFirstColumn: number[] = [];
 
 csvParser({ separator: '\t', headers: false });
 // csvParser(['sensor value']);
@@ -14,33 +14,51 @@ fs.createReadStream('2004.02.12.10.32.39')
   .pipe(csvParser())
   .on('data', (row) => results.push(row))
   .on('end', () => {
-    console.log(results);
-
-    let singleRow = '';
-    for (let i = 0; i < results.length; i++) {
-      singleRow = <string>results[i]['-0.049\t-0.071\t-0.132\t-0.010'];
-      let firstRowItem = singleRow.split('\t')[0];
-      rawDataFirstColumn.push(firstRowItem);
-    }
-
-    let firstRow = results[0]['-0.049\t-0.071\t-0.132\t-0.010'];
-
-    console.log('first row:');
-    console.log(firstRow);
-    console.log('first column:');
-    console.log(rawDataFirstColumn);
-
-    // console.log('raw data from first column:');
-    // console.log(rawDataFirstColumn);
+    // console.log(results);
     // [
     //   '-0.049\t-0.071\t-0.132\t-0.010': '-0.042\t-0.073\t-0.007\t-0.105'
     //   '-0.049\t-0.071\t-0.132\t-0.010': '0.015\t0.000\t0.007\t0.000'
     //    First column is just the first entry. Is currently getting ignored.
     // ]
+
+    let singleRow = '';
+    for (let i = 0; i < results.length; i++) {
+      if (i < 1000) {
+        singleRow = <string>results[i]['-0.049\t-0.071\t-0.132\t-0.010'];
+        let firstRowItem = singleRow.split('\t')[0];
+        let firstRowItemAsNum: number = +firstRowItem;
+        rawDataFirstColumn.push(firstRowItemAsNum);
+      }
+    }
+
+    console.log('first column:');
+    console.log(rawDataFirstColumn);
+
+    let rmsValueForFile = calculateRMS(rawDataFirstColumn);
+    console.log(rmsValueForFile);
+
+    // console.log('raw data from first column:');
+    // console.log(rawDataFirstColumn);
+
   });
 
 /* calculate rms value. see 4.1.2 of the full research doc (pg 9) for info on how this is calculated*/
+function calculateRMS(values) {
+  let currentTotal = 0;
 
+  // square
+  for (let i = 0; i < values.length; i++) {
+    currentTotal = currentTotal + Math.pow(values[i], 2);
+  }
+
+  // mean
+  currentTotal = currentTotal / values.length;
+
+  // root
+  currentTotal = Math.sqrt(currentTotal);
+
+  return currentTotal;
+}
 /*  threshold value for this data set is 542
     Check if rms value is above or below threshold */
 
