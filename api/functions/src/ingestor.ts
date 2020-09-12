3d130eea3443417f526683aa8fb16d56d886ddf1
Alex Monk <amonk446@gmail.com>
2020-09-12 15:45:23 +1200

    Added simple threshold detection and notification functions

--- api/functions/src/ingestor.ts
+++ api/functions/src/ingestor.ts
@@ -14,7 +14,6 @@ fs.createReadStream('2004.02.12.10.32.39')
   .pipe(csvParser())
   .on('data', (row) => results.push(row))
   .on('end', () => {
-    // console.log(results);
     // [
     //   '-0.049\t-0.071\t-0.132\t-0.010': '-0.042\t-0.073\t-0.007\t-0.105'
     //   '-0.049\t-0.071\t-0.132\t-0.010': '0.015\t0.000\t0.007\t0.000'
@@ -34,12 +33,9 @@ fs.createReadStream('2004.02.12.10.32.39')
     console.log('first column:');
     console.log(rawDataFirstColumn);
 
-    let rmsValueForFile = calculateRMS(rawDataFirstColumn);
-    console.log(rmsValueForFile);
-
-    // console.log('raw data from first column:');
-    // console.log(rawDataFirstColumn);
-
+    let rmsValueFromFile = calculateRMS(rawDataFirstColumn);
+    console.log(rmsValueFromFile);
+    thresholdDetection(rmsValueFromFile);
   });
 
 /* calculate rms value. see 4.1.2 of the full research doc (pg 9) for info on how this is calculated*/
@@ -59,8 +55,22 @@ function calculateRMS(values) {
 
   return currentTotal;
 }
-/*  threshold value for this data set is 542
+
+/*  threshold value for this data set is 542, will be updated with formula later
     Check if rms value is above or below threshold */
+function thresholdDetection(rmsValue) {
+  let thresholdValue = 542; //hardcoded rn
+  if (rmsValue >= thresholdValue){
+    console.log('Threshold met, notification being sent.');
+    sendNotification();
+  } else {
+    console.log('Threshold not met');
+  }
+}
+
+function sendNotification(){
+  // do stuff, possibly provide data on the sensor & machine
+}
 
 /* if over threshold, run notification() function */
 /* TODO add checker for notification frequency */
