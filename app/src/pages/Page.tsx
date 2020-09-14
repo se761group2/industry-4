c55b45b056d3dad6a5e97b4bdda8fbf1448df8f1
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-09-14 16:34:43 +1200

    Add LineGraph component

--- app/src/pages/Page.tsx
+++ app/src/pages/Page.tsx
@@ -16,45 +16,9 @@ import "./Page.css";
 import { from, useQuery } from "@apollo/client";
 import { GetUserById } from "../types/GetUserById";
 import { GET_USER_BY_ID } from "../common/graphql/queries/users";
-// import LineGraph from "../components/LineGraph";
-
 import { ellipsisHorizontal, ellipsisVertical, personCircle, search } from "ionicons/icons";
 
-import { CartesianGrid, DotProps, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
-
-type CustomDotProps = DotProps & { value?: number };
-
-const Dot: React.FC<CustomDotProps> = (props: CustomDotProps) => {
-    const { cx, cy, value } = props;
-
-    if (value && value > 500) {
-        return (
-            <svg x={(cx || 0) - 10} y={(cy || 0) - 14} height="20" width="20">
-                <polygon points="10,0 20,20 0,20" style={{ fill: "red" }} />
-                <text fill="#ffffff" fontSize="12" fontFamily="Verdana" x="8" y="17">
-                    !
-                </text>
-            </svg>
-        );
-    }
-
-    if (value && value > 400) {
-        return (
-            <svg x={(cx || 0) - 10} y={(cy || 0) - 14} height="20" width="20">
-                <polygon points="10,0 20,20 0,20" style={{ fill: "orange" }} />
-                <text fill="#ffffff" fontSize="12" fontFamily="Verdana" x="8" y="17">
-                    !
-                </text>
-            </svg>
-        );
-    }
-
-    return (
-        <svg x={(cx || 0) - 10} y={(cy || 0) - 10} width={20} height={20}>
-            <circle cx="10" cy="10" r="6" stroke="#8884df" strokeWidth="2" fill="white" />
-        </svg>
-    );
-};
+import LineGraph from "../components/LineGraph";
 
 const Page: React.FC = () => {
     const { name } = useParams<{ name: string }>();
@@ -62,13 +26,20 @@ const Page: React.FC = () => {
         variables: { id: "dummy" },
     });
     const data = [
-        { name: "Page A", value: 450 },
-        { name: "Page B", value: 650 },
-        { name: "Page C", value: 300 },
-        { name: "Page D", value: 225 },
-        { name: "Page E", value: 700 },
-        { name: "Page F", value: 650 },
-        { name: "Page G", value: 425 },
+        { name: "1", value: 350 },
+        { name: "2", value: 250 },
+        { name: "3", value: 300 },
+        { name: "4", value: 325 },
+        { name: "5", value: 400 },
+        { name: "6", value: 450 },
+        { name: "7", value: 425 },
+        { name: "8", value: 450 },
+        { name: "9", value: 650 },
+        { name: "10", value: 300 },
+        { name: "11", value: 425 },
+        { name: "12", value: 700 },
+        { name: "13", value: 650 },
+        { name: "14", value: 425 },
     ];
 
     return (
@@ -96,26 +67,7 @@ const Page: React.FC = () => {
             <IonContent color="new">
                 <div className="statusBar"></div>
                 <div className="graph">
-                    <LineChart
-                        width={600}
-                        height={300}
-                        data={data}
-                        margin={{ top: 20, right: 30, bottom: 5, left: 0 }}
-                        style={{ backgroundColor: "white" }}
-                    >
-                        <Line
-                            type="monotone"
-                            dataKey="uv"
-                            stroke="#8884df"
-                            isAnimationActive={false} // allows hollow dots to appear for unkown reason
-                            activeDot={{ r: 8 }}
-                            dot={<Dot />}
-                        />
-                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                    </LineChart>
+                    <LineGraph title="Sensor Values" redThreshold={600} yellowThreshold={400} data={data} />
                 </div>
                 <div className="download" style={{ textAlign: "center", margin: "20%" }}>
                     <IonButton
