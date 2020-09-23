e7328104324112f48dbd7e9371a71bd4904d083e
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-09-23 20:37:23 +1200

    Change spacing around health bar

--- app/src/components/HealthContainer.tsx
+++ app/src/components/HealthContainer.tsx
@@ -17,7 +17,7 @@ const HealthContainer: React.FC<ContainerProps> = ({ name, value, threshold }) =
     }
     return (
         <div className="flex justify-center text-black">
-            <div className={`rounded-lg shadow-xl font-bold text-lg responsive-width m-6 bg-${bg}-550`}>
+            <div className={`rounded-lg shadow-xl font-bold text-lg responsive-width m-4 bg-${bg}-550`}>
                 <div className="truncate text-left text-black float-left m-2 ml-4">{name}</div>
                 <div className="text-right text-black float-right m-2 mr-4">{value}</div>
             </div>
