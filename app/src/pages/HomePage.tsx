b23b2c2193e197f5ffeba1a5803ecc2e62805187
Raghav Garg <raghavgarg32@gmail.com>
2020-09-26 22:55:52 +1200

    Added buttons and text

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import {
     IonButton,
     IonButtons,
     IonContent,
+    IonFooter,
     IonHeader,
     IonIcon,
     IonImg,
@@ -13,13 +14,65 @@ import {
 import React from "react";
 import "./Pages.css";
 import gearsBg from "./images/gears-bg.jpg";
+import graph from "./images/graph.png";
 const HomePage: React.FC = () => {
     return (
         <IonPage>
             <link href="https://fonts.googleapis.com/css?family=Share Tech Mono" rel="stylesheet"></link>
-            <IonContent color="new">
-                <div className="ion-text-center"></div>
+            <IonContent color="new" style={{ width: "100%" }}>
+                <div className="text-center">
+                    <IonTitle className="text-center text-5xl font-heading ion-margin">Industry 4.0</IonTitle>
+                    <div className="info ion-margin" style={{ display: "block" }}>
+                        <div className="graphInfo">
+                            <div
+                                className="ion-text-justify text-lg"
+                                style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}
+                            >
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id fringilla metus.
+                                Aenean eget commodo elit, nec elementum enim. Aenean id tincidunt orci, eu malesuada
+                                diam. Cras id neque in libero consectetur suscipit in in nunc. Aliquam pharetra, mi non
+                                ultrices tincidunt, diam risus commodo sem, vitae semper ante elit vel mi
+                            </div>
+                            <div
+                                className="info responsive-width ion-margin"
+                                style={{ marginLeft: "auto", marginRight: "auto" }}
+                            >
+                                <IonImg src={graph}></IonImg>
+                            </div>
+                        </div>
+
+                        <div className="graphInfo">
+                            <div
+                                className="ion-text-start text-lg"
+                                style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}
+                            >
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id fringilla metus.
+                                Aenean eget commodo elit, nec elementum enim. Aenean id tincidunt orci, eu malesuada
+                                diam. Cras id neque in libero consectetur suscipit in in nunc. Aliquam pharetra, mi non
+                                ultrices tincidunt, diam risus commodo sem, vitae semper ante elit vel mi.
+                            </div>
+                            <div
+                                className="info responsive-width ion-margin"
+                                style={{ marginLeft: "auto", marginRight: "auto" }}
+                            >
+                                <IonImg src={graph}></IonImg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </IonContent>
+            <IonFooter>
+                <IonToolbar color="dark">
+                    <div className="text-center ">
+                        <IonButton shape="round" color="light" className="text-2xl normal-case ion-margin-end">
+                            Login
+                        </IonButton>
+                        <IonButton shape="round" color="light" className="text-2xl normal-case">
+                            Register
+                        </IonButton>
+                    </div>
+                </IonToolbar>
+            </IonFooter>
         </IonPage>
     );
 };
