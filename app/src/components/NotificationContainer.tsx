6247203b659b55d432a49492d1b2e02fd2583731
George-E-M <georgeedwinmcerlean@gmail.com>
2020-09-28 01:39:31 +1300

    Improved aesthetic

--- app/src/components/NotificationContainer.tsx
+++ app/src/components/NotificationContainer.tsx
@@ -9,24 +9,30 @@ interface ContainerProps {
 const NotificationContainer: React.FC<ContainerProps> = ({ type, handleAcknowledge, handleFixed }) => {
     return (
         <div className="flex justify-center text-white m-1 break-words pre">
-            <div className="text-center rounded-lg shadow-xl text-lg responsive-width m-6 bg-white pre">
+            <div className="text-center rounded-lg shadow-xl text-lg responsive-width m-6 bg-white pre border-4 border-red-500">
                 {type == "Acknowledgement" && (
                     <>
                         <div className="text-left text-black float-left m-2 ml-4 break-words pre">
                             This sensor has crossed its threshold indicating the machine needs servicing.
                         </div>
-                        <button className="text-center text-black float-none m-2 mr-4" onClick={handleAcknowledge}>
+                        <button
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold mb-3 py-2 px-4 rounded"
+                            onClick={handleAcknowledge}
+                        >
                             Acknowledge
                         </button>
                     </>
                 )}
                 {type == "Fixed" && (
                     <>
-                        <div className="text-left text-black float-left m-2 ml-4 break-words">
+                        <div className="text-left text-black float-left m-2 ml-4 break-words pre">
                             This machine/sensor is currently malfunctioning please fix the machine then mark it as fixed
                             to turn notifications back on.
                         </div>
-                        <button className="text-right text-black float-right m-2 mr-4" onClick={handleFixed}>
+                        <button
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold mb-3 py-2 px-4 rounded"
+                            onClick={handleFixed}
+                        >
                             Mark as Fixed
                         </button>
                     </>
