32e152e8e7f5df1b42a04ffe6a69fe5edc59d87f
Miles Erwin-Atmore <merw957@aucklanduni.ac.nz>
2020-09-27 23:57:05 +1300

    added 404 message

--- app/src/pages/Machines.tsx
+++ app/src/pages/Machines.tsx
@@ -24,6 +24,7 @@ import { getMachines } from "../types/getMachines";
 import { GET_MACHINES } from "../common/graphql/queries/machines";
 import { Link } from "react-router-dom";
 import ColourKey from "../components/ColourKey";
+import Error404 from "../components/ErrorMessage";
 
 const Machines: React.FC = () => {
     const machinesQuery = useQuery<getMachines>(GET_MACHINES);
@@ -39,18 +40,28 @@ const Machines: React.FC = () => {
             <Heading title="Industry 4.0" />
 
             <IonContent color="new">
-                <div className="responsive-width m-auto p-3">
-                    <ColourKey>Key</ColourKey>
-                </div>
-                <div className="responsive-width grid grid-cols-2 gap-5 m-auto pb-3">
-                    {machines?.map(function (machine) {
-                        return (
-                            <Link to={`/machine/${machine.id}`} key={machine.id}>
-                                <MachineContainer name={machine.name} health={machine.healthStatus} image={"random"} />
-                            </Link>
-                        );
-                    })}
-                </div>
+                {machines ? (
+                    <>
+                        <div className="responsive-width m-auto p-3">
+                            <ColourKey>Key</ColourKey>
+                        </div>
+                        <div className="responsive-width grid grid-cols-2 gap-5 m-auto pb-3">
+                            {machines?.map(function (machine) {
+                                return (
+                                    <Link to={`/machine/${machine.id}`} key={machine.id}>
+                                        <MachineContainer
+                                            name={machine.name}
+                                            health={machine.healthStatus}
+                                            image={"random"}
+                                        />
+                                    </Link>
+                                );
+                            })}
+                        </div>
+                    </>
+                ) : (
+                    <Error404 message="There are no machines." />
+                )}
                 <IonFab vertical="bottom" horizontal="center" slot="fixed">
                     <IonFabButton color="light">
                         <IonIcon icon={add} />
