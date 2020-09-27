e14229c896f12b1576abdcb36fb9af0592ae2e7f
Miles <48302893+merw957@users.noreply.github.com>
2020-09-28 01:02:50 +1300

    Machines page (#51)
    * create machinecontainer component
    
    * fixed missing key for iterator
    
    * changed images
    
    * moved font inside heading component
    
    * added 404 message
    
    * helper function for status/colour
    
    * added helper function to machinecontainer

--- app/src/components/Heading.tsx
+++ app/src/components/Heading.tsx
@@ -18,19 +18,22 @@ interface HeadingProps {
 
 const Heading: React.FC<HeadingProps> = ({ title }) => {
     return (
-        <IonHeader>
-            <IonToolbar color="dark">
-                <IonButtons slot="start">
-                    <IonBackButton />
-                </IonButtons>
-                <IonButtons slot="secondary">
-                    <IonButton>
-                        <IonIcon slot="icon-only" icon={personCircle} />
-                    </IonButton>
-                </IonButtons>
-                <IonTitle className="text-center text-2xl font-heading">{title ? title : "Industry 4.0"}</IonTitle>
-            </IonToolbar>
-        </IonHeader>
+        <>
+            <link href="https://fonts.googleapis.com/css?family=Share Tech Mono" rel="stylesheet"></link>
+            <IonHeader>
+                <IonToolbar color="dark">
+                    <IonButtons slot="start">
+                        <IonBackButton />
+                    </IonButtons>
+                    <IonButtons slot="secondary">
+                        <IonButton>
+                            <IonIcon slot="icon-only" icon={personCircle} />
+                        </IonButton>
+                    </IonButtons>
+                    <IonTitle className="text-center text-2xl font-heading">{title ? title : "Industry 4.0"}</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+        </>
     );
 };
 
---
+++ app/src/pages/Machines.tsx
@@ -0,0 +1,74 @@
+import {
+    IonBackButton,
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonFab,
+    IonFabButton,
+    IonHeader,
+    IonIcon,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+} from "@ionic/react";
+import React from "react";
+import { useParams } from "react-router";
+import MachineContainer from "../components/MachineContainer";
+import "./Page.css";
+import { from, useQuery } from "@apollo/client";
+import { add, ellipsisHorizontal, ellipsisVertical, personCircle, search } from "ionicons/icons";
+import Heading from "../components/Heading";
+import { Status } from "../types/globalTypes";
+import { getMachineById } from "../types/getMachineById";
+import { getMachines } from "../types/getMachines";
+import { GET_MACHINES } from "../common/graphql/queries/machines";
+import { Link } from "react-router-dom";
+import ColourKey from "../components/ColourKey";
+import Error404 from "../components/ErrorMessage";
+
+const Machines: React.FC = () => {
+    const machinesQuery = useQuery<getMachines>(GET_MACHINES);
+
+    // sort machines by health status
+    // (critcal, moderate, nominal) happens to be alphabetical so currently just sorting alphabetically
+    let machines = machinesQuery.data?.machines;
+    machines = machines?.slice().sort((a, b) => (a.healthStatus! > b.healthStatus! ? 1 : -1));
+
+    return (
+        <IonPage>
+            <Heading title="Industry 4.0" />
+
+            <IonContent color="new">
+                {machines ? (
+                    <>
+                        <div className="responsive-width m-auto p-3">
+                            <ColourKey>Key</ColourKey>
+                        </div>
+                        <div className="responsive-width grid grid-cols-2 gap-5 m-auto pb-3">
+                            {machines?.map(function (machine) {
+                                return (
+                                    <Link to={`/machine/${machine.id}`} key={machine.id}>
+                                        <MachineContainer
+                                            name={machine.name}
+                                            health={machine.healthStatus}
+                                            image={"random"}
+                                        />
+                                    </Link>
+                                );
+                            })}
+                        </div>
+                    </>
+                ) : (
+                    <Error404 message="There are no machines." />
+                )}
+                <IonFab vertical="bottom" horizontal="center" slot="fixed">
+                    <IonFabButton color="light">
+                        <IonIcon icon={add} />
+                    </IonFabButton>
+                </IonFab>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default Machines;
