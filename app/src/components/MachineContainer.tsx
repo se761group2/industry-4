05c7e3df6e93b6073f2d424ba4fd51f5dd88683b
Miles Erwin-Atmore <merw957@aucklanduni.ac.nz>
2020-09-28 00:46:10 +1300

    added helper function to machinecontainer

--- app/src/components/MachineContainer.tsx
+++ app/src/components/MachineContainer.tsx
@@ -6,6 +6,7 @@ import img3 from "../images/3.jpg";
 import img4 from "../images/4.jpg";
 import img5 from "../images/5.jpg";
 import { Status } from "../types/globalTypes";
+import { statusColour } from "../common/StatusColour";
 
 interface ContainerProps {
     name: string;
@@ -14,14 +15,7 @@ interface ContainerProps {
 }
 
 const MachineContainer: React.FC<ContainerProps> = ({ name, health, image }) => {
-    let bg: string;
-    if (health == "Nominal") {
-        bg = "green";
-    } else if (health == "Moderate") {
-        bg = "yellow";
-    } else {
-        bg = "red";
-    }
+    const bg: string = statusColour(health);
 
     const images: string[] = [img1, img2, img3, img4, img5];
     if (image == "random") {
