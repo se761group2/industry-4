839fb09fe45c8765ca858e4cd972de296bb8f3d0
George-E-M <georgeedwinmcerlean@gmail.com>
2020-09-28 01:21:26 +1300

    Fixed the backend update notification

--- api/functions/src/notifications/notificationService.ts
+++ api/functions/src/notifications/notificationService.ts
@@ -1,13 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 import sgMail from '@sendgrid/mail';
-import { firebaseApp } from '../firebase'
-// import config from '../../secrets.json';
-import { MachineStore } from '../graphql/MachineStore';
+import admin from 'firebase-admin';
+
+export const firebaseApp = admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://industry4-uoa.firebaseio.com',
+});
 
 const firestore = firebaseApp.firestore();
 
-// sgMail.setApiKey(config.SENDGRID_API_KEY);
+if (process.env.NODE_ENV == 'development') {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+}
+
+updateUsers();
 
 export function notifyUsers(threshold, recordedValue, sensorId, machineId) {
   const senderEmail = 'industry4errornotification@gmail.com';
@@ -36,12 +43,38 @@ export function notifyUsers(threshold, recordedValue, sensorId, machineId) {
   sgMail.send(msg);
 }
 
-export function updateUsers() {
+export async function updateUsers() {
   // Need to go into the db and get all sensors with problems to do this will need to update the schema,
   // To do that talk to Marc
-  const machines = MachineStore.getMachines();
+  const faultySensors = [] as any;
+  const machinesRef = firestore.collection('machines');
+  const machines = await machinesRef.get();
+  machines.forEach((machine) => {
+    machine.data().sensors.forEach((sensor) => {
+      if (sensor.notificationStatus == 'Unacknowledged') {
+        faultySensors.push({
+          machineName: machine.data().name,
+          sensorName: sensor.name,
+        });
+      }
+    });
+  });
 
-  // Get users associated with the failing sensors
+  let emailMsg = 'Issues have been detected with the following sensors:<br/>';
+
+  faultySensors.forEach((sensor) => {
+    emailMsg +=
+      'Sensor ' +
+      sensor.sensorName +
+      ', Machine ' +
+      sensor.machineName +
+      '<br/>';
+  });
+
+  emailMsg +=
+    'Please acknowledge and resolve these issues within the industry 4.0 application';
+  // This will be updated to link the the application when we have it deployed
+  emailMsg += '<a>Click here to visit the application</a>';
 
   // Notify users which of their sensors are failing
   const senderEmail = 'industry4errornotification@gmail.com';
@@ -51,13 +84,7 @@ export function updateUsers() {
     to: receiverEmail,
     from: senderEmail,
     subject: 'You have machine(s) with unacknowledged issues',
-    html:
-      'Issues have been detected with the following sensors:' +
-      'Sensor A, Machine B' +
-      'Sensor Q, Machine C' +
-      'Please acknowledge and resolve these issues within the industry 4.0 application' +
-      // This will be updated to link the the application when we have it deployed
-      '<a>Click here to visit the application</a>',
+    html: emailMsg,
   };
 
   sgMail.send(msg);
