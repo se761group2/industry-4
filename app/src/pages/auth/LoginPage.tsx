1f9afa91039a0955b7c84e027c96b3bca1715754
Raghav Garg <raghavgarg32@gmail.com>
2020-09-29 01:34:04 +1300

    Changed the styling of the landing page to match the theme

--- app/src/pages/auth/LoginPage.tsx
+++ app/src/pages/auth/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonPage } from "@ionic/react";
+import { IonContent, IonPage, IonTitle } from "@ionic/react";
 import firebase from "firebase";
 import React from "react";
 import { StyledFirebaseAuth } from "react-firebaseui";
@@ -20,12 +20,12 @@ export const LoginPage: React.FC<any> = () => {
     console.log("Rendering page");
     return (
         <IonPage>
-            <IonContent>
-                <div className="bg-gray-900 h-full w-full items-center flex justify-center flex-col">
+            <IonContent color="new">
+                <div className=" h-full w-full items-center flex justify-center flex-col">
                     <div className="text-center my-4">
-                        <h1 className="text-4xl text-gray-100 font-bold ">Industry 4.0</h1>
-                        <h2 className="text-2xl text-gray-300">
-                            <em>Monitor your machines like never before.</em>
+                        <IonTitle className="text-4xl text-gray-100 font-heading">Industry 4.0</IonTitle>
+                        <h2 className="text-2xl text-gray-300 font-heading">
+                            Monitor your machines like never before.
                         </h2>
                     </div>
                     <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
