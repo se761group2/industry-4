6e1d7acfc5d6930e7647de667bff1843470321cf
Raghav Garg <raghavgarg32@gmail.com>
2020-10-03 13:33:50 +1300

    Added edit button on each machine card

--- app/src/components/MachineContainer.tsx
+++ app/src/components/MachineContainer.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Component.css";
 import { Status } from "../types/globalTypes";
 import { statusColour } from "../common/StatusColour";
+import { IonFab, IonFabButton, IonIcon } from "@ionic/react";
+import { add, create } from "ionicons/icons";
 
 interface ContainerProps {
     name: string | undefined;
@@ -16,7 +18,12 @@ const MachineContainer: React.FC<ContainerProps> = ({ name, health, image }) => 
         <div className={`machine-container-1 rounded-lg shadow-xl m-auto bg-${bg}-550 darken-on-hover`}>
             <div className={"machine-container-2 rounded flex flex-col justify-between text-center bg-white"}>
                 <img className="machine-image rounded" src={image} alt="Machine image" />
-                <div className="text-black font-bold text-lg">{name}</div>
+                <div>
+                    <div className="text-black font-bold text-lg mt-3">{name}</div>
+                    <IonFabButton color="light" className="mr-auto ml-auto mt-5 mb-3" onClick={() => alert("hello")}>
+                        <IonIcon icon={create} />
+                    </IonFabButton>
+                </div>
             </div>
         </div>
     );
