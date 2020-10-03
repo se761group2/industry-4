4c92c7917d712d173fb9766e6e034219aa8d1d2b
Raghav Garg <raghavgarg32@gmail.com>
2020-10-03 15:55:26 +1300

    Placed edit button on top left corner

--- app/src/components/MachineContainer.tsx
+++ app/src/components/MachineContainer.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Component.css";
 import { Status } from "../types/globalTypes";
 import { statusColour } from "../common/StatusColour";
 import { IonFab, IonFabButton, IonIcon } from "@ionic/react";
 import { add, create } from "ionicons/icons";
+import { Link } from "react-router-dom";
+import { AddMachineModal } from "../pages/modals/AddMachineModal";
 
 interface ContainerProps {
     name: string | undefined;
@@ -13,18 +15,24 @@ interface ContainerProps {
 
 const MachineContainer: React.FC<ContainerProps> = ({ name, health, image }) => {
     const bg: string = statusColour(health);
+    const [addMachineOpen, setAddMachineOpen] = useState<boolean>(false);
 
     return (
-        <div className={`machine-container-1 rounded-lg shadow-xl m-auto bg-${bg}-550 darken-on-hover`}>
-            <div className={"machine-container-2 rounded flex flex-col justify-between text-center bg-white"}>
-                <img className="machine-image rounded" src={image} alt="Machine image" />
-                <div>
-                    <div className="text-black font-bold text-lg mt-3">{name}</div>
-                    <IonFabButton color="light" className="mr-auto ml-auto mt-5 mb-3" onClick={() => alert("hello")}>
-                        <IonIcon icon={create} />
-                    </IonFabButton>
+        <div>
+            <AddMachineModal open={addMachineOpen} setOpen={setAddMachineOpen} />
+            <IonFabButton color="light" className="absolute m-5" onClick={() => setAddMachineOpen(true)}>
+                <IonIcon icon={create} />
+            </IonFabButton>
+            <Link to={`/machine`}>
+                <div className={`machine-container-1 rounded-lg shadow-xl m-auto bg-${bg}-550`}>
+                    <div className={"machine-container-2 rounded flex flex-col justify-between text-center bg-white"}>
+                        <img className="machine-image rounded" src={image} alt="Machine image" />
+                        <div>
+                            <div className="text-black font-bold text-lg mt-3">{name}</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
