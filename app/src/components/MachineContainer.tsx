45b147a1d822cd7255ff2681f6f35ec6ea9a5ee3
Miles Erwin-Atmore <merw957@aucklanduni.ac.nz>
2020-10-04 18:42:21 +1300

    removed unnecessary assignment

--- app/src/components/MachineContainer.tsx
+++ app/src/components/MachineContainer.tsx
@@ -12,11 +12,6 @@ interface ContainerProps {
 const MachineContainer: React.FC<ContainerProps> = ({ name, health, image }) => {
     const bg: string = statusColour(health);
 
-    if (!image) {
-        image = undefined;
-        // image = images[Math.floor(Math.random() * images.length)];
-    }
-
     return (
         <div className={`machine-container-1 rounded-lg shadow-xl m-auto bg-${bg}-550 darken-on-hover`}>
             <div className={"machine-container-2 rounded flex flex-col justify-between text-center bg-white"}>
