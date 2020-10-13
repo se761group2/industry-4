b022c2216b2398b552bda35e784bf066c03ce3be
Frank Wang Ma <frankwangma@gmail.com>
2020-10-13 22:30:08 +1300

    Regenerated queries in the front end

--- app/src/common/graphql/queries/machines.ts
+++ app/src/common/graphql/queries/machines.ts
@@ -9,6 +9,11 @@ export const GET_MACHINES = gql`
             notificationStatus
             healthStatus
             image
+            sensors {
+                id
+                name
+                healthStatus
+            }
         }
     }
 `;
