9444f774f79a3653c26224ad37502a9e329bf410
George-E-M <georgeedwinmcerlean@gmail.com>
2020-10-14 21:19:21 +1300

    Removed unused log statements

--- api/functions/src/notifications/notificationService.ts
+++ api/functions/src/notifications/notificationService.ts
@@ -7,8 +7,6 @@ import * as functions from 'firebase-functions';
 const SENDGRID_API_KEY = functions.config().sendgrid.apikey;
 const firestore = firebaseApp.firestore();
 
-console.log(SENDGRID_API_KEY);
-
 sgMail.setApiKey(SENDGRID_API_KEY || process.env.SENDGRID_API_KEY);
 
 export async function notifyUsers(
